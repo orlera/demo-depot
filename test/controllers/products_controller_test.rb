a3bb327c031fa49a9a84d245c413090639bf5fd9
Andrea Orler <andrea@codegram.com>
2020-02-12 12:11:39 +0100

    Add cart in session model and store it in session

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -47,6 +47,14 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to products_url
   end
 
+  test "should not destroy product when line items are referenced" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url
+  end
+
   test "should get and validate index" do
     get products_url
     assert_response :success
