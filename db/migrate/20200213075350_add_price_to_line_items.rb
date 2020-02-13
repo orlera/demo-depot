05689d878903bfce7c8d20db329e4b60cdeef667
Andrea Orler <andrea@codegram.com>
2020-02-13 09:14:07 +0100

    Add price to line_item and copy it from product

---
+++ db/migrate/20200213075350_add_price_to_line_items.rb
@@ -0,0 +1,10 @@
+class AddPriceToLineItems < ActiveRecord::Migration[6.0]
+  def change
+    add_column :line_items, :price, :decimal, precision: 8, scale: 2
+
+    LineItem.all.each do |line_item|
+      line_item.price = line_item.product.price
+      line_item.save!
+    end
+  end
+end
